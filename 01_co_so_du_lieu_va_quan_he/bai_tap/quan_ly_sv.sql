d01b60e41cd920a6b0fe3178e4fd1c4d83201f25
phikhanhb7 <phikhanhb7@gmail.com>
2021-06-10 08:44:49 +0700

    up

---
+++ 01_co_so_du_lieu_va_quan_he/bai_tap/quan_ly_sv.sql
@@ -0,0 +1,9 @@
+USE student_management ;
+CREATE TABLE Teacher(
+id INT ,
+name varchar(50),
+age INT ,
+country varchar(50));
+INSERT INTO Teacher 
+VALUES 
+(1,Khanh,18, HaNoi );
---
+++ 01_co_so_du_lieu_va_quan_he/thuc_hanh/tao_table_SQL.sql
@@ -0,0 +1,1 @@
+CREATE TABLE student_management
